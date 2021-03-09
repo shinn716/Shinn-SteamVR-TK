ddcde43e9cc2d776fc0da8a30e68f26eb6c7bee0
John_tsai <john.tc.tsai@deltaww.com>
2021-03-09 08:50:46 +0800

    Add fadeback sample.

---
+++ Assets/Scenes/VR_FadeBack/FadebackManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadebackManager : MonoBehaviour
+{
+    ScreenFade screenFade;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        screenFade = new ScreenFade();
+        screenFade.FadeIn(1);
+        yield return new WaitForSeconds(1);
+        screenFade.FadeOut(1);
+        yield return new WaitForSeconds(1);
+        screenFade.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ScreenFade
+{
+    private GameObject m_go;
+    private Mesh m_mesh;
+    private Material m_materialInstance;
+    private int m_materialFadeID;
+    private float m_value;
+
+    public ScreenFade()
+    {
+        var mat = Resources.Load<Material>("Materials/FadeBlack");
+        m_mesh = new Mesh();
+        Vector3[] verts = new Vector3[]
+        {
+            new Vector3(-1, -1, 0),
+            new Vector3(-1, 1, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(1, -1, 0)
+        };
+        int[] elts = new int[] { 0, 1, 2, 0, 2, 3 };
+        m_mesh.vertices = verts;
+        m_mesh.triangles = elts;
+        m_mesh.RecalculateBounds();
+
+        m_go = new GameObject("fadeback", typeof(MeshFilter), typeof(MeshRenderer));
+        m_materialInstance = new Material(mat);
+
+        m_go.GetComponent<MeshFilter>().mesh = m_mesh;
+        m_go.GetComponent<MeshRenderer>().material = m_materialInstance;
+
+        m_go.transform.SetParent(Camera.main.transform);
+        m_go.transform.localPosition = Vector3.forward * .4f;
+        m_go.transform.localRotation = Quaternion.identity;
+        m_go.transform.localScale = Vector3.one;
+
+        m_materialFadeID = Shader.PropertyToID("_Fade");
+        m_materialInstance.SetFloat(m_materialFadeID, 0);
+    }
+
+    public void FadeIn(float during)
+    {
+        DOTween.To(() => m_value, x => m_value = x, 1, during).SetEase(Ease.OutSine).OnUpdate(() =>
+        m_materialInstance.SetFloat(m_materialFadeID, m_value)
+        );
+    }
+
+    public void FadeOut(float during)
+    {
+        DOTween.To(() => m_value, x => m_value = x, 0, during).SetEase(Ease.OutSine).OnUpdate(() =>
+        m_materialInstance.SetFloat(m_materialFadeID, m_value)
+        );
+    }
+
+    public void Dispose()
+    {
+        GameObject.Destroy(m_go);
+    }
+}
