316bf86c420059552e10cec475392da7dcb25760
John_tsai <john.tc.tsai@deltaww.com>
2021-04-16 14:23:29 +0800

    Add vr-colorpick.

---
+++ Assets/Scenes/VR_ColorPick/ColorPick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPick : MonoBehaviour
+{
+    public Transform TransformUIParent;
+    public Image ImageColorBoad;
+    public Image ImageColorPanel;
+    public Transform thumb;
+    
+    void Update()
+    {
+        SetThumbPosition(Standalone_Controller.instance.sprDot.transform.position);
+    }
+
+    private void SetThumbPosition(Vector3 point)
+    {
+        thumb.position = point;
+        GetColor(point);
+    }
+
+    private void GetColor(Vector2 pos)
+    {
+        var px = Map(pos.x - TransformUIParent.position.x, -.2f, .2f, 0, 730);
+        var py = Map(pos.y - TransformUIParent.position.y, -.1f, .3f, 0, 730);
+
+        Rect rect = ImageColorPanel.GetComponent<RectTransform>().rect;
+        Color imageColor = ImageColorPanel.sprite.texture.GetPixel(
+            Mathf.FloorToInt(px),
+            Mathf.FloorToInt(py) + 50
+            );
+        
+        ImageColorBoad.color = new Color(imageColor.r, imageColor.g, imageColor.b);
+    }
+
+    private float Map(float v, float a, float b, float x, float y)
+    {
+        return (v == a) ? x : (v - a) * (y - x) / (b - a) + x;
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 <img src="https://github.com/shinn716/VRUguiSample/blob/main/gif/drag01.gif" /></a>    
  - Vertical menu  
 <img src="https://github.com/shinn716/VRUguiSample/blob/main/gif/drag02.gif" /></a>  
+ - Color pick  
+<img src="https://github.com/shinn716/VRUguiSample/blob/main/gif/colorpick.gif" /></a>  
